f376d348560527201b680ac6c54a667e79527d18
ralphZero <placideralph@gmail.com>
2023-04-18 22:11:50 -0400

    add firebase to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    FirebaseModule,
+    TodoModule,
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
